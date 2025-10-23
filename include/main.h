c7a26c2da3fbfc94f96ae37c5e8ffcf0f7bacc41
willofdaedalus <manny.is.daedalus@pm.me>
2025-10-23 12:40:03 +0000

    fixed timer and adc init

--- include/main.h
+++ include/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define VREF (5.0) // 5V
+#define ADC_CONST (1023.0)
+
 #define PLAYER1_ADC (0x0) // pin A5
 #define PLAYER2_ADC (0x7) // pin A0
 
--- main.c
+++ main.c
@@ -5,40 +5,48 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// track which player's input we're reading
+volatile static uint8_t cur_player;
+volatile static uint8_t adc_read;
+
+
 enum GameState {
 	GS_TITLE,
 	GS_MAIN,
 	GS_SCORE,
 };
 
+ISR(ADC_vect)
+{
+	adc_read = ADCL | (ADCH << 8);
+	cur_player = !cur_player;
+}
+
 // initialize the adc interface to take readings of the potmeter
 // this function doesn't set the default analogue channel selection
 // check read_input for that
-void adc_init(void)
-{
-	// enable adc, auto trigger and interrupts
-	// ADCSRA |= _BV(ADEN) | _BV(ADATE) | _BV(ADIE);
-	// enable adc, auto trigger
-	ADCSRA |= _BV(ADEN) | _BV(ADATE);
-	// enable a prescaler of 128 giving us ~125kHz; perfect for adc
-	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
-	// auto trigger on timer/counter0 comp match
-	ADCSRB = _BV(ADTS0) | _BV(ADTS2);
-	// set reference selection to VCC
-	ADMUX = _BV(REFS0);
-}
-
-// initialize timers; this will be useful for auto triggering in the adc
-// whenever ocr0a hit that 249 value
 void timer_init(void)
 {
-	// CTC mode, reset at OCR0A
-	TCCR0A = _BV(WGM01);
+	// CTC mode: WGM12 = 1
+	TCCR1A = 0;
+	TCCR1B = _BV(WGM12);
 	// prescaler 1024
-	TCCR0B = _BV(CS02) | _BV(CS00);
-	// compare match interrupt enable
-	// TIMSK0 = _BV(OCIE0A);
-	OCR0A = 249; // ~16ms
+	TCCR1B |= _BV(CS12) | _BV(CS10);
+	// ~150ms for every update to reduce analogue noise and excessive
+	// polling on the potmeters
+	OCR1A = 2344;
+}
+
+void adc_init(void)
+{
+	// enable adc, auto trigger
+	ADCSRA = _BV(ADEN) | _BV(ADATE);
+	// prescaler 128 (~125 kHz)
+	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+	// auto trigger source: timer/counter1 compare match b
+	ADCSRB = _BV(ADTS2) | _BV(ADTS0);
+	// reference VCC
+	ADMUX = _BV(REFS0);
 }
 
 int main(void)
