dedadd573aae91969487c4fd0376d81c712c97ea
willofdaedalus <manny.is.daedalus@pm.me>
2025-10-23 15:10:43 +0000

    adc reads but once; switching to ctc triggers without auto

--- include/main.h
+++ include/main.h
@@ -4,7 +4,7 @@
 #define VREF (5.0) // 5V
 #define ADC_CONST (1023.0)
 
-#define PLAYER1_ADC (0x0) // pin A5
-#define PLAYER2_ADC (0x7) // pin A0
+#define PLAYER1_ADC (0x7) // pin A0
+#define PLAYER2_ADC (0x0) // pin A5
 
 #endif
--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include "lcd.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stdlib.h>
 #include <util/delay.h>
 
 // track which player's input we're reading
-volatile static uint8_t cur_player;
-volatile static uint8_t adc_read;
+static volatile uint8_t player1;
+static volatile uint8_t cur_meter;
+static volatile uint16_t adc_read;
 
 
 enum GameState {
@@ -19,19 +21,20 @@ enum GameState {
 ISR(ADC_vect)
 {
 	adc_read = ADCL | (ADCH << 8);
-	cur_player = !cur_player;
+	// adc_read = ADCL | (ADCH << 8);
+	// player1 = !player1;
+	// // clear out the current meter being read
+	// ADMUX &= ~(cur_meter << 1);
+	// // switch to the next potmeter
+	// cur_meter = (player1) ? PLAYER1_ADC : PLAYER2_ADC;
+	// // activate the next meter for reading on the next update
+	// ADMUX |= (cur_meter << 1);
 }
 
-// initialize the adc interface to take readings of the potmeter
-// this function doesn't set the default analogue channel selection
-// check read_input for that
 void timer_init(void)
 {
-	// CTC mode: WGM12 = 1
-	TCCR1A = 0;
-	TCCR1B = _BV(WGM12);
-	// prescaler 1024
-	TCCR1B |= _BV(CS12) | _BV(CS10);
+	// prescaler 1024 with CTC
+	TCCR1B |= _BV(CS12) | _BV(CS10) | _BV(WGM12);
 	// ~150ms for every update to reduce analogue noise and excessive
 	// polling on the potmeters
 	OCR1A = 2344;
@@ -39,26 +42,65 @@ void timer_init(void)
 
 void adc_init(void)
 {
+	// enable adc, auto trigger and interrupts
+	// ADCSRA |= _BV(ADEN) | _BV(ADATE) | _BV(ADIE);
 	// enable adc, auto trigger
-	ADCSRA = _BV(ADEN) | _BV(ADATE);
-	// prescaler 128 (~125 kHz)
+	ADCSRA |= _BV(ADEN) | _BV(ADATE) | _BV(ADIE);
+	// enable a prescaler of 128 giving us ~125kHz; perfect for adc
 	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
-	// auto trigger source: timer/counter1 compare match b
+	// auto trigger on timer/counter1 comp match
 	ADCSRB = _BV(ADTS2) | _BV(ADTS0);
-	// reference VCC
-	ADMUX = _BV(REFS0);
+	// set reference selection to VCC
+	ADMUX = _BV(REFS0) | PLAYER1_ADC;
 }
+
+// initialize the adc interface to take readings of the potmeter
+// this function doesn't set the default analogue channel selection
+// check read_input for that
+// void timer_init(void)
+// {
+// 	// CTC mode: WGM12 = 1
+// 	TCCR1A = 0;
+// 	TCCR1B = _BV(WGM12);
+// 	// prescaler 1024
+// 	TCCR1B |= _BV(CS12) | _BV(CS10);
+// 	// ~150ms for every update to reduce analogue noise and excessive
+// 	// polling on the potmeters
+// 	OCR1A = 2344;
+// }
+//
+// void adc_init(void)
+// {
+// 	// enable adc, auto trigger
+// 	ADCSRA = _BV(ADEN) | _BV(ADATE);
+// 	// prescaler 128 (~125 kHz)
+// 	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+// 	// auto trigger source: timer/counter1 compare match b
+// 	ADCSRB = _BV(ADTS2) | _BV(ADTS0);
+// 	// reference VCC
+// 	ADMUX = _BV(REFS0);
+//
+// 	// very important line; player one always starts first and we switch
+// 	// everytime OCR1A hits its MAX to balance both potmeters on the same
+// 	// ADC line thanks to the multiplexer
+// 	ADMUX |= (PLAYER1_ADC << 1);
+// }
 
 int main(void)
 {
-	enum GameState state = GS_TITLE;
+	enum GameState state = GS_MAIN;
+	char buf[8] = {0};
 
 	_delay_ms(2000);
 	i2c_init();
 	lcd_init();
 
-	adc_init();
 	timer_init();
+	adc_init();
+
+	// player1 = 1;
+	// // starting meter is player1
+	// cur_meter = PLAYER1_ADC;
 
 
 	sei();
@@ -73,17 +115,25 @@ int main(void)
 			lcd_display("@willofdaedalus");
 			_delay_ms(2000);
 			state = GS_MAIN;
+			lcd_clear();
 			break;
 
 		case GS_MAIN:
+			itoa(adc_read, buf, 10);
+			lcd_set_cursor(0, 0);
+			lcd_display("     ");
+			lcd_set_cursor(0, 0);
+			lcd_display(buf);
 			// draw the two players
 			// read input and convert
-			lcd_display("main loop!");
+			// lcd_display("main loop!");
 			break;
 
 		case GS_SCORE:
+			// draw the two players
+			// read input and convert
+			// lcd_display("main loop!");
 			break;
 		}
 	}
-	return 0;
 }
